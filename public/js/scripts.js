cb73dfdc158a28b2d3db904ff03522210363e052
Alvin James Bellero <alvinb@Zyluns-MacBook-Pro.local>
2017-04-21 12:34:36 +0800

    Updated app structure and code :art

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,28 +6,44 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   var username = 'Guest',
+      firebaseLogo = document.getElementById('chat-logo'),
       textMessage = document.getElementById('txt-message'),
       sendButton = document.getElementById('send-button'),
       usernameElm = document.getElementsByClassName('username-elem'),
       googleLogin = document.getElementById('google-login'),
       signOut = document.getElementById('sign-out'),
+      googleLoginBack = document.getElementById('google-login-back'),
+      signOutBack = document.getElementById('sign-out-back'),
+      uploadPhoto = document.getElementById('new-upload'),
+      uploadPhotoImg = document.getElementById('image-icon'),
+      fileElem = document.getElementById('file'),
       messagesList = document.getElementById('messages');
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyB5SOdRZ_Y0pDnqTHru1Igtv2Cb2ZniimA",
-    authDomain: "fir-dev-meetup-demo.firebaseapp.com",
-    databaseURL: "https://fir-dev-meetup-demo.firebaseio.com",
-    projectId: "fir-dev-meetup-demo",
-    storageBucket: "fir-dev-meetup-demo.appspot.com",
-    messagingSenderId: "524117790237"
+  // Control firebase logo loading
+  var setLoading = (load) => {
+    if(load)
+      firebaseLogo.classList.add('loading');
+    else
+      firebaseLogo.classList.remove('loading');
   };
 
-  firebase.initializeApp(config);
-  console.info('Firebase Initialized!')
+  var checkIfImage = (chat, li) => {
+    if ( chat.message.indexOf("https://firebasestorage.googleapis.com/") == 0
+      || chat.message.indexOf("https://lh3.googleusercontent.com/") == 0
+      || chat.message.indexOf("http://pbs.twimg.com/") == 0
+      || chat.message.indexOf("data:image/") == 0) {
+      var imgElm = document.createElement("img");
+      imgElm.src = chat.message;
+      li.appendChild(imgElm);
+    } else {
+      var messageElm = document.createElement("span");
+      messageElm.innerText = chat.message;
+      li.appendChild(messageElm);
+    }
+  };
 
   // Create elements and append new message to chat box
-  let addMessage = (chat) => {
+  var addMessage = (chat) => {
     let li = document.createElement('li');
     let nameElm = document.createElement('h4');
     nameElm.innerText = chat.name;
@@ -36,42 +52,57 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let messageElm = document.createElement('div');
     messageElm.innerText = chat.message;
-    li.appendChild(messageElm);
-
+    checkIfImage(chat, li);
     messagesList.appendChild(li);
     li.scrollIntoView(false);
 
     textMessage.value = '';
   };
 
+  // Handle showing/ hiding elements
+  var handleElementState = (isLoggedIn, newUsername) => {
+    usernameElm[0].innerText = usernameElm[1].innerText = newUsername;
+    usernameElm[0].style.display = usernameElm[1].style.display = 'inline-block';
+    signOut.style.display = signOutBack.style.display = isLoggedIn ? 'inline-block' : 'none';
+    googleLogin.style.display = googleLoginBack.style.display = isLoggedIn ? 'none' : 'inline-block';
+
+  };
+
   // Set Username Method
-  let setUsername = (newUsername) => {
+  var setUsername = (newUsername) => {
     if (newUsername == null) { newUsername = 'Guest'; }
     username = newUsername;
-    let isLoggedIn = username != 'Guest';
-    usernameElm[0].innerText = usernameElm[1].innerText = newUsername;
-    signOut.style.display = isLoggedIn ? '' : 'none';
-    googleLogin.style.display = isLoggedIn ? 'none' : '';
+    var isLoggedIn = username != 'Guest';
+    handleElementState(isLoggedIn, newUsername);
   }
+
+  // Handle upload click
+  uploadPhoto.addEventListener('click', (e) => {
+    fileElem.click();
+  });
+
+  uploadPhotoImg.addEventListener('click', (e) => {
+    fileElem.click();
+  });
 
   // Keypress Event for message input text
   textMessage.addEventListener('keypress', e => {
-    let message = textMessage.value;
+    var message = textMessage.value;
     if(e.keyCode == 13 && message !== '') {
       sendButton.click();
     }
   });
 
-  // Click Event for Send button
+  //===========================================================
+
+  // Click event to add message to chat box
   sendButton.addEventListener('click', e => {
-    let message = textMessage.value;
+    var message = textMessage.value;
     if(message === '') return false;
-    addMessage({name: 'Guest', message: message});
+    var chat = {name: username, message: message};
+    addMessage(chat);
   });
-  
+
   setUsername('Guest');
-  // Add reference to database child
-  // var dbRef = firebase.database().ref().child('chat');
-  // dbRef.on('value', snap => console.log(snap.val()));
 
 });
